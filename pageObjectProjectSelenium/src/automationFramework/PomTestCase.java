82aa1495e30c7f592649a26ef565bb85329c986b
grzegorzlobos <grzegorz.lobos84@gmail.com>
2015-11-07 18:43:51 +0100

    adding Repo.prop, modifying PomTestCase

--- pageObjectProjectSelenium/src/automationFramework/PomTestCase.java
+++ pageObjectProjectSelenium/src/automationFramework/PomTestCase.java
@@ -3,7 +3,9 @@ package automationFramework;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import pageObjectUtils.*;
-
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class PomTestCase {
 
@@ -15,13 +17,24 @@ public class PomTestCase {
 			WebDriver driver = InitFactoryPattern.initBrowser("Firefox");  //new Fireforx driver instance 
 		    driver.get("http://www.allegro.pl");							//--created by Factory pattern
 		    driver.manage().window().maximize();
-		    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);		    
-		    MainPage.goToMyAccount(driver,"close","zaloguj").click();
-		    LoginPage.userLoginPassword(driver,"close","username","password");
-		    LoginPage.loginAccount(driver,".//*[@id='auth-form']/div[9]/div/button").click();	    
-		    MainPage.leaveMyAccount(driver,"wyloguj").click();
-		    driver.quit();
-		   
+		    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);	
+		    
+		    try{	    			 
+		    Properties objProp = new Properties();
+		    FileInputStream objfile = new FileInputStream("src/automationFramework/Repo.properties");
+		    objProp.load(objfile);
+		    
+		    MainPage.goToMyAccount(driver,objProp.getProperty("1"),objProp.getProperty("2")).click();
+		    LoginPage.userLoginPassword(driver,objProp.getProperty("1"),objProp.getProperty("3"),objProp.getProperty("4"));
+		    LoginPage.loginAccount(driver,objProp.getProperty("5")).click();	    
+		    MainPage.leaveMyAccount(driver,objProp.getProperty("6")).click();
+		    }
+		    catch (IOException e) {
+				e.printStackTrace();
+		    }
+		    finally{
+		    	driver.quit();
+		    }
 		    }
 
     }
