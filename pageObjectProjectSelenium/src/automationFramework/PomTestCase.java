9e404511819bf68cefa059a874a7aa31e2cc5126
grzegorzlobos <grzegorz.lobos84@gmail.com>
2015-11-19 13:38:49 +0100

    modified POMtestCase

--- pageObjectProjectSelenium/src/automationFramework/PomTestCase.java
+++ pageObjectProjectSelenium/src/automationFramework/PomTestCase.java
@@ -6,35 +6,39 @@ import pageObjectUtils.*;
 import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.IOException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class PomTestCase {
 
 
-	public static void main(String[] args) {
-		
-			
-		
-			WebDriver driver = InitFactoryPattern.initBrowser("Firefox");  //new Fireforx driver instance 
-		    driver.get("http://www.allegro.pl");							//--created by Factory pattern
-		    driver.manage().window().maximize();
-		    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);	
-		    
-		    try{	    			 
+			private WebDriver driver = null;
+
+    		@BeforeClass
+    		public void initialize() {
+    			WebDriver driver = InitFactoryPattern.initBrowser("Firefox");  
+    		    driver.get("http://www.allegro.pl");							
+    		    driver.manage().window().maximize();
+    		    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    		}
+					
+    		
+    		@Test
+    		public void testing() throws IOException{
+    			
 		    Properties objProp = new Properties();
 		    FileInputStream objfile = new FileInputStream("src/automationFramework/Repo.properties");
-		    objProp.load(objfile);
-		    
+		    objProp.load(objfile);	    
 		    MainPage.goToMyAccount(driver,objProp.getProperty("1"),objProp.getProperty("2")).click();
 		    LoginPage.userLoginPassword(driver,objProp.getProperty("1"),objProp.getProperty("3"),objProp.getProperty("4"));
 		    LoginPage.loginAccount(driver,objProp.getProperty("5")).click();	    
 		    MainPage.leaveMyAccount(driver,objProp.getProperty("6")).click();
 		    }
-		    catch (IOException e) {
-				e.printStackTrace();
-		    }
-		    finally{
-		    	driver.quit();
-		    }
-		    }
-
+    		
+    		
+    	    @AfterClass
+    	    public void cleaning(){
+		    driver.quit();
+    	    }
     }
