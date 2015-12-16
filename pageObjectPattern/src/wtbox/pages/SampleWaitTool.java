ce20c571443262594c3714eb3e6434c54e6d5619
grzegorzlobos <grzegorz.lobos84@gmail.com>
2015-12-16 20:16:07 +0100

    add sample wait methods

---
+++ pageObjectPattern/src/wtbox/pages/SampleWaitTool.java
@@ -0,0 +1,186 @@
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SampleWaitTool {
+
+
+	public static final int DEFAULT_WAIT_4_ELEMENT = 7; 
+	public static final int DEFAULT_WAIT_4_PAGE = 12; 
+
+	public static WebElement waitForElement(WebDriver driver, final By by, int timeOutInSeconds) {
+		WebElement element; 
+		try{	
+			
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+			WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds); 
+			element = wait.until(ExpectedConditions.visibilityOfElementLocated(by));	
+			driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); //reset implicitlyWait
+			return element; //return the element	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return null; 
+	}
+	
+	
+	
+	public static WebElement waitForElementPresent(WebDriver driver, final By by, int timeOutInSeconds) {
+		WebElement element; 
+		try{
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+			
+			WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds); 
+			element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
+			
+			driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); //reset implicitlyWait
+			return element; //return the element
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return null; 
+	}
+	
+
+	public static List<WebElement> waitForListElementsPresent(WebDriver driver, final By by, int timeOutInSeconds) {
+		List<WebElement> elements; 
+		try{	
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+			  
+			WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds); 
+			wait.until((new ExpectedCondition<Boolean>() {
+	            @Override
+	            public Boolean apply(WebDriver driverObject) {
+	                return areElementsPresent(driverObject, by);
+	            }
+	        }));
+			
+			elements = driver.findElements(by); 
+			driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); //reset implicitlyWait
+			return elements; //return the element	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return null; 
+	}
+
+	 public static WebElement waitForElementRefresh(WebDriver driver, final By by, 
+			                           int timeOutInSeconds) {
+		WebElement element; 
+		try{	
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+		        new WebDriverWait(driver, timeOutInSeconds) {
+		        }.until(new ExpectedCondition<Boolean>() {
+
+		            @Override
+		            public Boolean apply(WebDriver driverObject) {
+		                driverObject.navigate().refresh(); //refresh the page ****************
+		                return isElementPresentAndDisplay(driverObject, by);
+		            }
+		        });
+		    element = driver.findElement(by);
+			driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); //reset implicitlyWait
+			return element; //return the element
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return null; 
+	 }
+	 
+
+	public static boolean waitForTextPresent(WebDriver driver, final By by, final String text, int timeOutInSeconds) {
+		boolean isPresent = false; 
+		try{	
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+	        new WebDriverWait(driver, timeOutInSeconds) {
+	        }.until(new ExpectedCondition<Boolean>() {
+	
+	            @Override
+	            public Boolean apply(WebDriver driverObject) {
+	            	return isTextPresent(driverObject, by, text); //is the Text in the DOM
+	            }
+	        });
+	        isPresent = isTextPresent(driver, by, text);
+			driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); //reset implicitlyWait
+			return isPresent; 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return false; 
+	}
+	
+
+	
+	public static void nullifyImplicitWait(WebDriver driver) {
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+	} 
+	
+
+	public static void setImplicitWait(WebDriver driver, int waitTime_InSeconds) {
+		driver.manage().timeouts().implicitlyWait(waitTime_InSeconds, TimeUnit.SECONDS);  
+	} 
+	
+	public static void resetImplicitWait(WebDriver driver) {
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+		driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); //reset implicitlyWait
+	} 
+	
+
+
+	public static void resetImplicitWait(WebDriver driver, int newWaittime_InSeconds) {
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //nullify implicitlyWait() 
+		driver.manage().timeouts().implicitlyWait(newWaittime_InSeconds, TimeUnit.SECONDS); //reset implicitlyWait
+	} 
+		    
+
+
+	private static boolean isTextPresent(WebDriver driver, By by, String text)
+	{
+		try {
+				return driver.findElement(by).getText().contains(text);
+		} catch (NullPointerException e) {
+				return false;
+		}
+	}
+		
+	private static boolean isElementPresent(WebDriver driver, By by) {
+		try {
+			driver.findElement(by);//if it does not find the element throw NoSuchElementException, which calls "catch(Exception)" and returns false; 
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+	
+	private static boolean areElementsPresent(WebDriver driver, By by) {
+		try {
+			driver.findElements(by); 
+			return true; 
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private static boolean isElementPresentAndDisplay(WebDriver driver, By by) {
+		try {			
+			return driver.findElement(by).isDisplayed();
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+	
+
+	
+ }
+
